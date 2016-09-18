8ee5b4d055e01404b048e8b3c1206f41fccef999
thadeaus <enfinityfilms@gmail.com>
2016-09-17 20:30:56 -0400

    Created Day/Night Scenes to cycle through
    In progress

---
+++ Assets/Scripts/DayChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayChange : MonoBehaviour {
+
+    public Material NightSkybox;
+    public Material DaySkybox;
+	
+    void Start()
+    {
+        SetDay();
+    }
+
+    void SetDay() {
+        RenderSettings.skybox = DaySkybox;
+        GameObject.Find("Stars").SetActive(false);
+        GameObject.Find("Firefly").SetActive(false);
+        GameObject.Find("Moon").GetComponent<Shader>()
+        GameObject.Find("Spotlight").SetActive(false);
+        GameObject.Find("SunLight").SetActive(true);
+    }
+
+}
