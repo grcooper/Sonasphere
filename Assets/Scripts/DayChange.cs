f77b17d6d1eb0014b1e3e07771c1643171ad6019
thadeaus <enfinityfilms@gmail.com>
2016-09-17 22:18:22 -0400

    Changed Skybox and edited the beach

--- Assets/Scripts/DayChange.cs
+++ Assets/Scripts/DayChange.cs
@@ -5,19 +5,70 @@ public class DayChange : MonoBehaviour {
 
     public Material NightSkybox;
     public Material DaySkybox;
-	
-    void Start()
+
+    public enum FadingState { OFF, OUT, IN };
+    private FadingState fadingState = FadingState.OFF;
+
+    void Start ()
     {
         SetDay();
     }
 
-    void SetDay() {
+    public void SetDay() {
         RenderSettings.skybox = DaySkybox;
         GameObject.Find("Stars").SetActive(false);
         GameObject.Find("Firefly").SetActive(false);
-        GameObject.Find("Moon").GetComponent<Shader>()
         GameObject.Find("Spotlight").SetActive(false);
         GameObject.Find("SunLight").SetActive(true);
+    }
+
+    public void SetNight()
+    {
+        RenderSettings.skybox = NightSkybox;
+        GameObject.Find("Stars").SetActive(true);
+        GameObject.Find("Firefly").SetActive(true);
+        GameObject.Find("Spotlight").SetActive(true);
+        GameObject.Find("SunLight").SetActive(false);
+    }
+
+
+    public Texture2D fadeTexture;
+    private float fadeSpeed = 1f;
+    private int drawDepth = -1000;
+
+    private float alpha = 1.0f;
+    private float fadeDir = -1f;
+
+    void OnGUI()
+    {
+        if (fadingState == FadingState.OUT)
+        {
+            alpha -= fadeDir * fadeSpeed * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+
+            Color thisAlpha = GUI.color;
+            thisAlpha.a = alpha;
+            GUI.color = thisAlpha;
+
+            GUI.depth = drawDepth;
+
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+        }
+        else if(fadingState == FadingState.IN)
+        {
+            alpha += fadeDir * fadeSpeed * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+
+            Color thisAlpha = GUI.color;
+            thisAlpha.a = alpha;
+            GUI.color = thisAlpha;
+
+            GUI.depth = drawDepth;
+
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+        }
+
+
     }
 
 }
